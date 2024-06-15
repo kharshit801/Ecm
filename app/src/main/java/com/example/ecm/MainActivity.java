5f51c2896d8356a4711362f20c0f5c1a3a27b327
Shanu-Kumawat <shanukumawat01@gmail.com>
2024-06-15 12:58:47 +0530

    home pages added

--- app/src/main/java/com/example/ecm/MainActivity.java
+++ app/src/main/java/com/example/ecm/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ecm;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,27 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText nameEditText;
+    EditText regNoEditText;
+    Button startButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        nameEditText = findViewById(R.id.et_name);
+        regNoEditText = findViewById(R.id.et_reg_no);
+        startButton = findViewById(R.id.btn_start);
+
+        startButton.setOnClickListener(v -> {
+            String userName = nameEditText.getText().toString();
+            Intent intent = new Intent(MainActivity.this, Homescreen.class);
+            intent.putExtra("USER_NAME", userName);
+            startActivity(intent);
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
